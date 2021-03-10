e482a8437dbed5aff5a636b53e1daa093a52998a
ksvedal2 <ksvedal@gmail.com>
2021-03-10 07:48:29 +0100

    Lastet opp v2 av Lege.java (med utvidelser).

--- Lege.java
+++ Lege.java
@@ -1,26 +1,49 @@
-class Lege{
+class Lege implements Comparable<Lege>{
 
   String lege;
+  LenketListe<Resept> utskrevedeResepter;
 
-  public Lege(String navn){
+  public Lege(String navn) {
     lege = navn;
+    utskrevedeResepter = new LenketListe<Resept>();
   }
 
-  public String hentLege(){
+  public void kastUlovligUtskrift(Lege l, Legemidler lm) throws UlovligUtskrift {
+    if (!(l instanceof Spesialist) && (lm instanceof Narkotisk)) {
+        throw new UlovligUtskrift(l, lm);
+    }
+  }
+
+  public LenketListe<Resept> hentUtskrevedeResepter() {
+    return utskrevedeResepter;
+  }
+
+  @Override
+  public int compareTo(Lege l) {
+    return lege.compareTo(l.hentLege());
+  }
+
+  public String hentLege() {
     return lege;
   }
+
+  public void skrivUtLegesNavn() {
+    System.out.println("Legens navn: " + hentLege());
+  }
+
 }
 
-class Spesialist extends Lege implements Godkjenningsfritak{
+
+class Spesialist extends Lege implements Godkjenningsfritak {
 
   int kontrollId;
 
-  public Spesialist(String navn, int kontroll){
+  public Spesialist(String navn, int kontroll) {
   super(navn);
   kontrollId = kontroll;
   }
 
-  public int hentKontrollId(){
+  public int hentKontrollId() {
     return kontrollId;
   }
 }
