8aa5bf5c46a458edf2dd9a1630d305cb44413e06
theam <theamathilde@gmail.com>
2021-03-12 15:31:43 +0100

    Hovedprogram med ny meny for legg til element

--- Hovedprogram.java
+++ Hovedprogram.java
@@ -50,4 +50,32 @@ public class Hovedprogram {
       System.out.println("  0: Programmet avslutter.");
       System.out.println("Skriv inn tall til valget ditt: ");
     }
+
+    static void leggTilElementMeny(){
+      System.out.println("Du faar naa fire valg." + "\n" +
+      "For aa legge til en pasient, tast '1'" + "\n" +
+      "For aa legge til en lege, tast '2'" +"\n" +
+      "For aa legge til en resept, tast '3'"+ "\n" +
+      "For aa legge til et legemiddel, tast '4'" + "\n" +
+      "For aa avslutte, tast 0.");
+
+      while (in != 0){
+
+        switch(in) {
+
+          case 1:
+
+            leggTilPasient();
+            break;
+
+        }
+
+        switch (in){
+          case 2:
+           
+        }
+      }
+    }
+
+
 }
