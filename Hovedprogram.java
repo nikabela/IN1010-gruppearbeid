f36d9e3778c6fd3fb26864a6d2ab8651b5cf3170
Anton Lymarev <anton.lymarev.1998@gmail.com>
2021-03-12 14:14:45 +0100

    moderniserte litt kommandoløkken; nå kan E3 ble kalt direkte fra den

--- Hovedprogram.java
+++ Hovedprogram.java
@@ -3,32 +3,51 @@ import java.io.*;
 
 public class Hovedprogram {
     public static void main(String[] args) throws UlovligFormat, UlovligUtskrift {
-        Legesystem legesystem = new Legesystem();
-        legesystem.lesInnFraFil("inndata.txt");
+      Legesystem legesystem = new Legesystem();
+      legesystem.lesInnFraFil("inndata.txt");
 
-        System.out.println("Du har foelgende valg:");
-        System.out.println("1: Du kan skrive ut en fullstendig oversikt over pasienter, leger, legemidler og resepter  \n");  //peker til deloppgave E3
-        System.out.println("2: Du kan opprette og legge til nye elementer i systemet \n"); //peker til deloppgave E4
-        System.out.println("3: Du kan bruke en gitt resept fra listen til en pasient   \n"); //peker til deloppgave E5
-        System.out.println("4: Du kan skrive ut forskjellige former for statistikk  \n"); //peker til deloppgave E6
-        System.out.println("5: Skrive alle data til fil \n"); //peker til deloppgave E7
-        System.out.println("0: programmet avslutter. \n ");
-        System.out.println("Skriv inn tall til valget ditt: ");
+      startMeny(); // kaller paa start meny (se nederst)
 
-        Scanner valg = new Scanner(System.in);
-        int in = valg.nextInt();
+      Scanner valg = new Scanner(System.in);
+      int in = valg.nextInt();
 
-        while(in != 0){             //her skal programmet hoppe til riktig deloppgave i E
-          if(in == 1){
-          }else if (in ==2){
-          }else if(in == 3){
-          }else if(in == 4){
-          }else if(in == 5){
-          }else{
-            System.out.println("ugyldig innput : ");
+      // selve kommandoloekken
+      while(in != 0){             //her skal programmet hoppe til riktig deloppgave i E
+        if(in == 1){
+          System.out.println("\n\nEn fullstendig oversikt over pasienter, leger, legemidler og resepter:");
+          legesystem.hentOversikt();
+
+          // "gaa-tilbake"-blokken; skal bare kopieres til andre deler av denne if-sjekken naar disse er skrevet ferdig
+          System.out.println("Skriv inn 9 for aa gaa tilbake til start meny: ");
+          int inp = valg.nextInt();
+          while (inp != 9) {
+            System.out.println("Ugyldig input!");
+            inp = valg.nextInt();
           }
-          break;
+          if (inp == 9) {startMeny(); in = valg.nextInt();}
         }
-        valg.close();
+        else if (in ==2){
+        }else if(in == 3){
+        }else if(in == 4){
+        }else if(in == 5){
+        }else{
+          System.out.println("Ugyldig input!");
+        }
+        break;
+      }
+      valg.close();
+      System.out.println("\nAvslutter programmet. Ha det!");
+    }
+
+
+    static void startMeny() {
+      System.out.println("Du har foelgende valg:");
+      System.out.println("  1: Du kan skrive ut en fullstendig oversikt over pasienter, leger, legemidler og resepter");  //peker til deloppgave E3
+      System.out.println("  2: Du kan opprette og legge til nye elementer i systemet"); //peker til deloppgave E4
+      System.out.println("  3: Du kan bruke en gitt resept fra listen til en pasient"); //peker til deloppgave E5
+      System.out.println("  4: Du kan skrive ut forskjellige former for statistikk"); //peker til deloppgave E6
+      System.out.println("  5: Skrive alle data til fil"); //peker til deloppgave E7
+      System.out.println("  0: Programmet avslutter.");
+      System.out.println("Skriv inn tall til valget ditt: ");
     }
 }
