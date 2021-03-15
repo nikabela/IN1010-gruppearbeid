503bf63716cfa65036b1db7b5f598a781b21cfbc
ksvedal2 <ksvedal@gmail.com>
2021-03-15 20:57:12 +0100

    Endelig versjon LenketListe.java for levering.

--- LenketListe.java
+++ LenketListe.java
@@ -37,17 +37,11 @@ public class LenketListe <T> implements Liste<T>{
     @Override
     public T next(){return liste.hent(indeks++);}
   }
-  
-  
-/*    @Override
-    public void remove(){}
-  }
-  */ 
-  
+
   public Iterator<T> iterator(){
     return new LenkelisteIterator(this);
-  }  
-  
+  }
+
   @Override
   public String toString(){
     Node denne = forste;
@@ -82,32 +76,23 @@ public class LenketListe <T> implements Liste<T>{
 
     // hvis ugyldig indeks
     if(pos < 0 || pos > stoerrelse()){
-
       throw new UgyldigListeIndeks(pos);
     }
 
   // om lista er tom
     else if (forste == null){
-
-
         forste = siste = nyNode;
         forste.forrige = null;
         siste.neste = null;
-
     }
 
     else if (pos == 0){
-      // System.out.println("wtf");
-
       forste.forrige = nyNode;
       nyNode.neste = forste;
       nyNode.forrige = null;
       forste = nyNode;
 
-
-
     }  else if (pos == stoerrelse()){
-
       siste.neste = nyNode;
       nyNode.forrige = siste;
       siste = nyNode;
@@ -145,39 +130,27 @@ public class LenketListe <T> implements Liste<T>{
   }
 
   public void sett(int pos, T x){
-
-
     Node denne = forste;
     Node sist = siste;
-
     if(pos <0 || pos > stoerrelse()-1 ){
       throw new UgyldigListeIndeks(pos);
     }
 
     if (pos == 0){
-
       denne.data = x;
-
     }
     else if (pos == stoerrelse()){
-
       System.out.println(sist);
       sist.data = x;
-
-
+    } else{
+        for (int i= 0; i < pos && denne.neste != null; i++){
+          denne = denne.neste;
     }
-
-    else{
-      for (int i= 0; i < pos && denne.neste != null; i++){
-      denne = denne.neste;
-    }
-    denne.data = x;
-
+     denne.data = x;
   }
 }
 
   public T hent(int pos){
-
     Node denne = forste;
     Node sist = siste;
 
@@ -187,7 +160,6 @@ public class LenketListe <T> implements Liste<T>{
 
     // hvis liste er tom
     if (denne == null){
-
       throw new UgyldigListeIndeks(pos);
     }
     else if (pos == 0){
@@ -205,18 +177,16 @@ public class LenketListe <T> implements Liste<T>{
   }
   // // denne metoden fjerner og returnerer elementet på starten av lista
   public T fjern(){
-
     Node denne = forste;
     // om lista er tom gis en feilmelding
     if (denne == null){
       throw new UgyldigListeIndeks(-1);
     }
+
     if (forste == siste){
       siste = null;
-
     }
      else {
-
       forste.neste.forrige = null;
     }
     forste = forste.neste;
@@ -230,35 +200,24 @@ public class LenketListe <T> implements Liste<T>{
     Node tmp = siste;
     Node fjern;
 
-
     if (pos < 0 || pos > stoerrelse() -1){
       throw new UgyldigListeIndeks(pos);
     }
 
     else if (pos == stoerrelse() -1  && denne.neste != null){
-
       fjern = siste;
-
       siste = siste.forrige;
       siste.neste = null;
-
       return fjern.data;
-
-
     } else  if (pos == 0 ) {
-
-
       return fjern();
-
     }
      else {
       for (int i = 0; denne != null && i < pos; i++ ){
         denne = denne.neste;
       }
       denne.forrige.neste = denne.neste;
-
       denne.neste.forrige = denne.forrige;
-
     } return denne.data;
     }
   }
